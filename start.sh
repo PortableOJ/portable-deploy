c2b3dfa5e6098d09ad251691a1aba8d1c7caca7f
shiroha <keqing.hu@icloud.com>
2022-03-04 14:55:41 +0800

    更新清理时间

--- start.sh
+++ start.sh
@@ -20,6 +20,11 @@ sudo docker-compose up -d
 
 echo "等待服务就绪"
 
+# 由于启动需要一定时间，所以这段时间内对镜像进行清理
+echo "开始清理过期镜像"
+
+sudo docker image ls | grep portable | grep none | awk '{print $3}' | xargs sudo docker rmi
+
 ./wait-for localhost:8080 -- echo "服务已经就绪"
 
 code=`curl localhost:8080/api/judge/initCode`
@@ -42,9 +47,5 @@ url=`ifconfig -a | grep inet | grep -v 127.0.0.1 | grep -v inet6 | awk '{print $
 wd=`pwd`
 
 sudo docker run -itd --name portable-deploy_judge_1 -e home=/portable_data -e serverUrl=${url} -e heartbeatTime=5 -e serverCode=${code} -v ${wd}/data:/portable_data --log-opt max-size=10m --log-opt max-file=5 998244353/portable-judge
-
-echo "开始清理过期镜像"
-
-sudo docker image ls | grep portable | grep none | awk '{print $3}' | xargs sudo docker rmi
 
 echo "启动完成"
