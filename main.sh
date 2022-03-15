b3986d97a1ffb99f82e8c34112619d8915dc971c
shiroha <keqing.hu@icloud.com>
2022-03-15 09:13:47 +0800

    增加了 UI 界面

--- main.sh
+++
@@ -1,9 +0,0 @@
-echo "正在更新启动脚本(master)"
-
-sudo git pull origin master
-
-echo "正在启动"
-
-sudo chmod +x ./start-server
-
-./start-server
---
+++ start.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+tput clear
+tput civis
+
+w=60
+cols=`tput cols`
+
+if [ $cols -lt 60 ]
+then
+    echo "the windows is too small to show the UI"
+    exit 1
+fi
+
+left=`expr $cols - $w`
+left=`expr $left / 2`
+
+pre=""
+space=" "
+for i in $(seq 1 $left)
+do
+    pre="${pre}${space}"
+done
+
+tput clear
+
+echo -e "${pre}\033[36m+\033[36m*********************************************************\033[36m+"
+echo -e "${pre}\033[36m|\033[34m  _____           _        _     _         ____       _  \033[36m|"
+echo -e "${pre}\033[36m|\033[34m |  __ \         | |      | |   | |       / __ \     | | \033[36m|"
+echo -e "${pre}\033[36m|\033[34m | |__) |__  _ __| |_ __ _| |__ | | ___  | |  | |    | | \033[36m|"
+echo -e "${pre}\033[36m|\033[34m |  ___/ _ \| '__| __/ _  | '_ \| |/ _ \ | |  | |_   | | \033[36m|"
+echo -e "${pre}\033[36m|\033[34m | |  | (_) | |  | || (_| | |_) | |  __/ | |__| | |__| | \033[36m|"
+echo -e "${pre}\033[36m|\033[34m |_|   \___/|_|   \__\__,_|_.__/|_|\___|  \____/ \____/  \033[36m|"
+echo -e "${pre}\033[36m|\033[34m ======================================================= \033[36m|"
+echo -e "${pre}\033[36m|\033[34m                                                         \033[36m|"
+echo -e "${pre}\033[36m|\033[35m    copyright @ Portable Online Judge Team      (v0.1.0) \033[36m|"
+echo -e "${pre}\033[36m|\033[35m    (https://github.com/PortableOJ)                      \033[36m|"
+echo -e "${pre}\033[36m|\033[34m                                                         \033[36m|"
+echo -e "${pre}\033[36m|\033[31m     Open source based on GNU GENERAL PUBLIC LICENSE     \033[36m|"
+echo -e "${pre}\033[36m+\033[36m*********************************************************\033[36m*"
+echo -e "\033[0m"
+
+line=15
+col=`expr ${left} + 10`
+select=0
+
+showOption() {
+    tput cup `expr ${line} + $1` ${col}
+    if [[ $1 == $select ]]
+    then
+        echo -en " \033[35m*\033[0m "
+    else
+        echo -n "   "
+    fi
+    echo $2
+}
+
+while [[ true ]]; do
+    showOption 0 "Start a Server with a Judge."
+    showOption 1 "Start a Server without Judge."
+    showOption 2 "Start a remote Judge."
+    showOption 3 "Update this shell and restart."
+    showOption 4 "Do nothing and quit."
+
+    tput cup `expr ${line} + 6` `expr ${col} - 3`
+    echo -e "(use 'w/s' to select, use 'enter' to confirm)"
+
+    read -sn1 action
+    if [ -z $action ]
+    then
+        break
+    fi
+    case $action in
+        w | W)
+            select=`expr ${select} - 1`
+            ;;
+        s | S)
+            select=`expr ${select} + 1`
+            ;;
+    esac
+done
+
+case $select in
+    0)
+        sudo chmod +x ./start-server
+        ./start-server Y
+        ;;
+    1)
+        sudo chmod +x ./start-server
+        ./start-server N
+        ;;
+    3)
+        sudo git pull origin master
+        ./start
+        ;;
+    4)
+        ;;
+esac
+
+tput cnorm
