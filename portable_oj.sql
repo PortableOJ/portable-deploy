3c0553634de362629cf41ad625beffacfab0b678
shiroha <keqing.hu@icloud.com>
2022-03-16 16:32:51 +0800

    更新 SQL

--- portable_oj.sql
+++ portable_oj.sql
@@ -18,6 +18,22 @@ SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
 -- ----------------------------
+-- Table structure for batch
+-- ----------------------------
+DROP TABLE IF EXISTS `batch`;
+CREATE TABLE `batch` (
+  `id` bigint unsigned NOT NULL AUTO_INCREMENT COMMENT '主键 ID',
+  `owner` bigint NOT NULL COMMENT '拥有者 ID',
+  `contest_id` bigint DEFAULT NULL COMMENT '绑定至的比赛 ID',
+  `prefix` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '用户名前缀',
+  `count` int NOT NULL COMMENT '总数量',
+  `ip_lock` tinyint(1) NOT NULL COMMENT '是否锁定 IP',
+  `status` varchar(32) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL COMMENT '状态',
+  PRIMARY KEY (`id`),
+  KEY `idx_owner` (`owner`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
 -- Table structure for contest
 -- ----------------------------
 DROP TABLE IF EXISTS `contest`;
