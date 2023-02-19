b6eb81ef732e8b5951e58a0a920666dbf70dc076
shiroha <keqing.hu@icloud.com>
2023-02-19 15:36:41 +0800

    更新签名

--- portable_oj.sql
+++ portable_oj.sql
@@ -1,19 +1,3 @@
-/*
- Navicat Premium Data Transfer
-
- Source Server         : Localhost_MySQL
- Source Server Type    : MySQL
- Source Server Version : 80028
- Source Host           : localhost:3306
- Source Schema         : portable_oj
-
- Target Server Type    : MySQL
- Target Server Version : 80028
- File Encoding         : 65001
-
- Date: 01/03/2022 16:31:37
-*/
-
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
